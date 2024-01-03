00b7e68c57f6dc093ba500628e6da4c8c4c2e99a
Caleb Lam <caleb.lam@hotmail.com>
2024-01-03 00:00:31 -0500

    Initialize project

---
+++ src/main/java/com/resumeholic/api/ResumeholicApiApplication.java
@@ -0,0 +1,13 @@
+package com.resumeholic.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeholicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeholicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resumeholic/api/ResumeholicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumeholic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeholicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
